ac3d7eccc6f468faa07883848d5d785a6ed801f1
Chris <chriswell.mangahas@gmail.com>
2025-06-27 00:19:36 +0800

    Added the first class for the code, and the initilizing def method, initilizing the world.

--- mine_blocks.py
+++ mine_blocks.py
@@ -1,2 +1,8 @@
 from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
+
+class World(Entity):
+    def __init__(self, size_x=50, size_z=50):
+        super().__init__()
+        self.boxes = []
+        self.build_world(size_x, size_z)
