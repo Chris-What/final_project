5970db08edd5c9b8f201c3cd1b7f8d6d5007caf0
Chris <chriswell.mangahas@gmail.com>
2025-06-27 00:13:37 +0800

    Added file for the code of the program.

---
+++ mine_blocks.py
@@ -0,0 +1,10 @@
+from ursina import *
+from ursina.prefabs.first_person_controller import FirstPersonController
+
+class World(Entity):
+    def __init__(self, size_x=50, size_z=50):
+        super().__init__()
+        self.boxes = []
+        self.build_world(size_x, size_z)
+
+    def build_world(self, size_x, size_z)
