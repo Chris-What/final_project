2cadf067d601ad375d4327844f1fcd0b547cc461
Chris <chriswell.mangahas@gmail.com>
2025-06-27 00:24:54 +0800

    Added second def method for class World; the look of the world.

--- mine_blocks.py
+++ mine_blocks.py
@@ -6,3 +6,16 @@ class World(Entity):
         super().__init__()
         self.boxes = []
         self.build_world(size_x, size_z)
+
+    def build_world(self, size_x, size_z):
+        for block in range(size_x):
+            for cube in range(size_z):
+                box = Button(
+                    color=color.white,
+                    model="cube",
+                    position=(cube, 0, block),
+                    texture="grass.png",
+                    parent=self,
+                    origin_y=0.5
+                )
+                self.boxes.append(box)
